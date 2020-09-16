9739d93b80057829deee5c1a54f205efb83a31ac
aybe <aybe.one@hotmail.com>
2020-09-16 23:44:38 +0200

    Initial commit

---
+++ .prev/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+
+// ReSharper disable CheckNamespace
+// ReSharper disable IdentifierTypo
+// ReSharper disable StringLiteralTypo
+internal static class Program
+{
+    // ReSharper disable once RedundantAssignment
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Passed arguments: " + args.Length);
+
+        foreach (var arg in args)
+        {
+            Console.WriteLine($"\t{arg}");
+        }
+
+        Console.WriteLine("Press a key to continue");
+        Console.ReadKey();
+
+//#if DEBUG
+//        // ReSharper disable once RedundantAssignment
+//        // ReSharper disable StringLiteralTypo
+//        args = new[] {@"C:\temp\dosbox\QBASIC.EXE"};
+//        args = new[] {@"C:\temp\dosbox\ski32.exe"};
+//        // ReSharper restore StringLiteralTypo
+//#endif
+
+        if (args.Length != 0)
+        {
+            var fileName  = args[0];
+            var arguments = string.Join(" ", args.Skip(1));
+
+            if (!Start(fileName))
+            {
+                Console.WriteLine("Starting program directly");
+                Process.Start(fileName, arguments);
+            }
+        }
+
+        Console.WriteLine("Press a key to continue");
+        Console.ReadKey();
+    }
+
+    private static bool Start(string fileName)
+    {
+        var key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\DOSBox-X");
+
+        if (key == null)
+            return false;
+
+        if (!(key.GetValue("Location") is string dosbox))
+            return false;
+
+        if (!NativeMethods.GetBinaryType(fileName, out var fileType))
+            return false;
+
+        if (fileType != NativeConstants.SCS_DOS_BINARY)
+            return false;
+
+        var arguments = $@"-fastlaunch -defaultdir ""{Path.GetDirectoryName(dosbox)}"" ""{fileName}""";
+
+        var info = new ProcessStartInfo(dosbox, arguments)
+        {
+            UseShellExecute = true
+        };
+
+        Console.WriteLine("Starting program with DOSBox");
+        Process.Start(info);
+
+        return true;
+    }
+}
+
+internal static class NativeMethods
+{
+    [DllImport("Kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static extern bool GetBinaryType(string lpApplicationName, out uint lpBinaryType);
+}
+
+[SuppressMessage("ReSharper", "InconsistentNaming")]
+[SuppressMessage("ReSharper", "UnusedMember.Global")]
+internal static class NativeConstants
+{
+    public const int ERROR_BAD_EXE_FORMAT = 193;
+    public const int SCS_32BIT_BINARY     = 0;
+    public const int SCS_DOS_BINARY       = 1;
+    public const int SCS_WOW_BINARY       = 2;
+    public const int SCS_PIF_BINARY       = 3;
+    public const int SCS_POSIX_BINARY     = 4;
+    public const int SCS_OS216_BINARY     = 5;
+    public const int SCS_64BIT_BINARY     = 6;
+}
---
+++ dosbox-x-launcher/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+
+// ReSharper disable CheckNamespace
+// ReSharper disable IdentifierTypo
+// ReSharper disable InconsistentNaming
+// ReSharper disable StringLiteralTypo
+// ReSharper disable CommentTypo
+
+internal static class Program
+{
+    private const string HKCR = "HKEY_CURRENT_USER";
+    private const int SCS_DOS_BINARY = 1;
+
+    private static void SetupKeyDefaultValue(string keyName, string valueName)
+    {
+        if (string.IsNullOrWhiteSpace(keyName))
+            throw new ArgumentException("Value cannot be null or whitespace.", nameof(keyName));
+
+        if (string.IsNullOrWhiteSpace(valueName))
+            throw new ArgumentException("Value cannot be null or whitespace.", nameof(valueName));
+
+        var value = Registry.GetValue(keyName, null, null);
+        if (value as string != valueName)
+        {
+            Registry.SetValue(keyName, null, valueName);
+        }
+    }
+
+    private static void Main(string[] args)
+    {
+        if (args.Length > 0 && args[0] == "-u")
+        {
+            return; // TODO uninstall everything
+        }
+
+        var dosbox      = GetDosBoxPath();
+        var launcher    = $@"{HKCR}\Software\Classes\Applications\{AppDomain.CurrentDomain.FriendlyName}";
+        var launcherKey = launcher.Substring(launcher.IndexOf("Applications", StringComparison.OrdinalIgnoreCase));
+
+        // SetupKeyDefaultValue($@"{launcher}\DefaultIcon",            @"""%1""");
+        SetupKeyDefaultValue($@"{launcher}\shell\open\command",     $@"""{Assembly.GetExecutingAssembly().Location}"" ""%1"" %2 %3 %4 %5 %6 %7 %8 %9");
+        SetupKeyDefaultValue($@"{HKCR}\Software\DOSBox-X\Location", dosbox);
+        SetupKeyDefaultValue(@$"{HKCR}\Software\Classes\.com",      launcherKey);
+        SetupKeyDefaultValue(@$"{HKCR}\Software\Classes\.exe",      launcherKey);
+
+        if (args.Length <= 0)
+            return;
+
+        var fileName = args[0];
+        var fileType = Path.GetExtension(fileName);
+        var fileArgs = string.Join(" ", args.Skip(1));
+
+        Registry.SetValue($@"{HKCR}\Software\Classes\{fileType}", null, $"{fileType.Substring(1)}file");
+
+        if (dosbox != null && IsMsDosExecutable(fileName))
+        {
+            fileName = dosbox;
+            fileArgs = $@"-fastlaunch -defaultdir ""{Path.GetDirectoryName(dosbox)}"" ""{fileName}""";
+        }
+
+        Process.Start(fileName, fileArgs);
+
+        Registry.SetValue($@"{HKCR}\Software\Classes\{fileType}", null, launcherKey);
+    }
+
+    private static string GetDosBoxPath()
+    {
+        var path = Assembly.GetExecutingAssembly().Location;
+
+        path = Path.GetDirectoryName(path) ?? throw new InvalidOperationException();
+        path = Path.Combine(path, "dosbox-x.exe");
+
+        if (File.Exists(path))
+        {
+            return path;
+        }
+
+        path = @"C:\DOSBox-X\dosbox-x.exe";
+
+        if (File.Exists(path))
+        {
+            return path;
+        }
+
+        return null;
+    }
+
+    private static bool IsMsDosExecutable(string path)
+    {
+        return NativeMethods.GetBinaryType(path, out var type) && type == SCS_DOS_BINARY;
+    }
+}
+
+internal static class NativeMethods
+{
+    [DllImport("Kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static extern bool GetBinaryType(string lpApplicationName, out uint lpBinaryType);
+}
