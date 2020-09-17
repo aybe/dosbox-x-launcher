78a960c1dfb4b8736f8366a442d659236add8717
Wengier <wengierwu@yahoo.com>
2020-09-16 21:15:05 -0400

    update handler

--- dosbox-x-launcher/dosbox-x-launcher.cs
+++ dosbox-x-launcher/dosbox-x-launcher.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Windows.Forms;
 using Microsoft.Win32;
 
 // ReSharper disable CheckNamespace
@@ -17,40 +18,50 @@ internal static class Program
     private const string HKCR = "HKEY_CURRENT_USER";
     private const int SCS_DOS_BINARY = 1;
 
-    private static void SetupKeyDefaultValue(string keyName, string valueName)
+    private static void SetupKeyDefaultValue(string keyName, string valueData, string valueName = null)
     {
         if (string.IsNullOrWhiteSpace(keyName))
-            throw new ArgumentException("Value cannot be null or whitespace.", nameof(keyName));
+            throw new ArgumentException("Key cannot be null or whitespace.", nameof(keyName));
 
-        if (string.IsNullOrWhiteSpace(valueName))
-            throw new ArgumentException("Value cannot be null or whitespace.", nameof(valueName));
+        if (string.IsNullOrWhiteSpace(valueData))
+            throw new ArgumentException("Data cannot be null or whitespace.", nameof(valueData));
 
-        var value = Registry.GetValue(keyName, null, null);
-        if (value as string != valueName)
+        var value = Registry.GetValue(keyName, valueName, null);
+        if (value as string != valueData)
         {
-            Registry.SetValue(keyName, null, valueName);
+            Registry.SetValue(keyName, valueName, valueData);
         }
     }
 
     private static void Main(string[] args)
     {
-        if (args.Length > 0 && args[0] == "-u")
+        if (args.Length > 0 && (args[0] == "/u" || args[0] == "-u"))
         {
-            return; // TODO uninstall everything
+            var keytree = Registry.CurrentUser.OpenSubKey($@"Software\Classes\Applications", true);
+            if (keytree != null && keytree.OpenSubKey($@"{AppDomain.CurrentDomain.FriendlyName}") != null)
+                keytree.DeleteSubKeyTree($@"{AppDomain.CurrentDomain.FriendlyName}");
+            SetupKeyDefaultValue($@"{HKCR}\Software\Classes\.com",      "comfile");
+            SetupKeyDefaultValue($@"{HKCR}\Software\Classes\.exe",      "exefile");
+            MessageBox.Show("DOSBox-X Launcher uninstalled.", "Message");
+            return;
         }
 
-        var dosbox      = GetDosBoxPath();
         var launcher    = $@"{HKCR}\Software\Classes\Applications\{AppDomain.CurrentDomain.FriendlyName}";
         var launcherKey = launcher.Substring(launcher.IndexOf("Applications", StringComparison.OrdinalIgnoreCase));
-
-        // SetupKeyDefaultValue($@"{launcher}\DefaultIcon",            @"""%1""");
+        var key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\DOSBox-X");
+        if (key == null || !(key.GetValue("Location") is string dosbox) || !File.Exists(dosbox)) {
+            dosbox      = GetDosBoxXPath();
+            SetupKeyDefaultValue($@"{HKCR}\Software\DOSBox-X", dosbox, "Location");
+        }
+        SetupKeyDefaultValue($@"{launcher}\DefaultIcon",            @"""%1""");
         SetupKeyDefaultValue($@"{launcher}\shell\open\command",     $@"""{Assembly.GetExecutingAssembly().Location}"" ""%1"" %2 %3 %4 %5 %6 %7 %8 %9");
-        SetupKeyDefaultValue($@"{HKCR}\Software\DOSBox-X\Location", dosbox);
-        SetupKeyDefaultValue(@$"{HKCR}\Software\Classes\.com",      launcherKey);
-        SetupKeyDefaultValue(@$"{HKCR}\Software\Classes\.exe",      launcherKey);
+        SetupKeyDefaultValue($@"{HKCR}\Software\Classes\.com",      launcherKey);
+        SetupKeyDefaultValue($@"{HKCR}\Software\Classes\.exe",      launcherKey);
 
-        if (args.Length <= 0)
+        if (args.Length <= 0) {
+            MessageBox.Show("DOSBox-X Launcher installed.", "Message");
             return;
+        }
 
         var fileName = args[0];
         var fileType = Path.GetExtension(fileName);
@@ -60,8 +71,8 @@ internal static class Program
 
         if (dosbox != null && IsMsDosExecutable(fileName))
         {
-            fileName = dosbox;
             fileArgs = $@"-fastlaunch -defaultdir ""{Path.GetDirectoryName(dosbox)}"" ""{fileName}""";
+            fileName = dosbox;
         }
 
         Process.Start(fileName, fileArgs);
@@ -69,7 +80,7 @@ internal static class Program
         Registry.SetValue($@"{HKCR}\Software\Classes\{fileType}", null, launcherKey);
     }
 
-    private static string GetDosBoxPath()
+    private static string GetDosBoxXPath()
     {
         var path = Assembly.GetExecutingAssembly().Location;
 
